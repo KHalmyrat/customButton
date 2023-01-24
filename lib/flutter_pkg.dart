6d4de2cdf4efc68871ae2fbc54532818aa0d305f
KHalmyrat <k.saparow@payhas.com>
2023-01-24 17:29:22 +0500

    onpress

--- lib/flutter_pkg.dart
+++ lib/flutter_pkg.dart
@@ -5,14 +5,13 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   var onPressed;
   final Widget child;
-  var style;
 
-  CustomButton(this.child, this.onPressed, this.style);
+  CustomButton(this.child, this.onPressed);
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-        onPressed: onPressed,
+        onPressed: (){print(onPressed);},
         style: TextButton.styleFrom(
           padding: const EdgeInsets.all(16.0),
           primary: Colors.white,
